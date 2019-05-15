475aec4b7dc641e2ce2ba57c11136db29c32bf74
Lyga Danylo <dlyg@keplercode.com>
2019-05-14 23:42:48 -0700

    function for printing matrix

--- src/components/fieldGame.js
+++ src/components/fieldGame.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class FieldGame extends Component {
+  qwe() {
+    const { inputValue } = this.props;
+    switch (inputValue) {
+    }
+  }
   render() {
     const { inputValue } = this.props;
     return <div>{inputValue}</div>;
